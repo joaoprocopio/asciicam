81299d1f38c180b44ae558e24971bd7c1723727c
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-01-30 18:17:10 +0000

    vai

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,21 @@ import "./styles/global.css";
 const ASCII_CHARS =
   "@&%QWNM0gB$#DR8mHXKAUbGOpV4d9h6PkqwSE2]ayjxY5Zoen[ult13If}C{iF|(7J)vTLs?z/*cr!+<>;=^,_:'-.`";
 
-console.log(ASCII_CHARS);
+const rootEl = document.getElementById("__root__");
+
+if (!rootEl) {
+  throw new Error("Root element not found");
+}
+
+const stream = await navigator.mediaDevices.getUserMedia({
+  video: true,
+});
+
+const videoEl = document.createElement("video");
+videoEl.classList.add("size-full");
+videoEl.srcObject = stream;
+videoEl.addEventListener("loadedmetadata", () => {
+  videoEl.play();
+});
+
+rootEl.appendChild(videoEl);
