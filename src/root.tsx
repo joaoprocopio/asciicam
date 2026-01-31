31a7f56321adca67d37518abf623f87fdf60b6f4
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-01-31 19:30:33 +0000

    vai

--- src/root.tsx
+++ src/root.tsx
@@ -1,8 +1,8 @@
-import { createCameras } from "@solid-primitives/devices";
 import { makeEventListener } from "@solid-primitives/event-listener";
+import createRAF from "@solid-primitives/raf";
 
 import type { Accessor } from "solid-js";
-import { createEffect } from "solid-js";
+import { createEffect, createResource, on, onMount } from "solid-js";
 
 const ASCII_CHARS =
   "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@";
@@ -11,15 +11,40 @@ const CHAR_WIDTH = 8;
 const CHAR_HEIGHT = 10;
 
 export function Root() {
-  const bufferEl = document.createElement("canvas");
-  const bufferCtx = bufferEl.getContext("2d", { willReadFrequently: true });
+  const [camera] = createResource(() =>
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+    }),
+  );
 
   let canvasEl!: HTMLCanvasElement;
   let canvasCtx!: CanvasRenderingContext2D;
 
-  const cameras = createCameras();
-  createEffect(() => {
-    console.log(cameras());
+  const bufferEl: HTMLCanvasElement = document.createElement("canvas");
+  const bufferCtx: CanvasRenderingContext2D = bufferEl.getContext("2d", {
+    willReadFrequently: true,
+  })!;
+
+  const videoEl: HTMLVideoElement = document.createElement("video");
+
+  const [loopRunning, startLoop, stopLoop] = createRAF(() => {
+    canvasCtx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+    bufferCtx.drawImage(videoEl, 0, 0, bufferEl.width, bufferEl.height);
+    const image = bufferCtx.getImageData(0, 0, bufferEl.width, bufferEl.height);
+    for (let i = 0; i < image.data.length; i += 4) {
+      const luminance =
+          0.2126 * image.data[i + 0] +
+          0.7152 * image.data[i + 1] +
+          0.0722 * image.data[i + 2],
+        charIndex = Math.floor((luminance / 255) * (ASCII_CHARS.length - 1)),
+        char = ASCII_CHARS[charIndex],
+        pixelIndex = i / 4,
+        pixelX = (pixelIndex % bufferEl.width) * CHAR_WIDTH,
+        pixelY = Math.floor(pixelIndex / bufferEl.width) * CHAR_HEIGHT;
+
+      canvasCtx.fillStyle = `#fff`;
+      canvasCtx.fillText(char, pixelX, pixelY);
+    }
   });
 
   makeCanvasResizer({
@@ -32,6 +57,27 @@ export function Root() {
     canvas: () => bufferEl,
     width: () => Math.floor(canvasEl.width / CHAR_WIDTH),
     height: () => Math.floor(canvasEl.height / CHAR_HEIGHT),
+  });
+
+  createEffect(() => {
+    if (
+      !(canvasEl instanceof HTMLElement) ||
+      !(bufferEl instanceof HTMLElement)
+    )
+      return;
+
+    canvasCtx.font = `${FONT_SIZE}px monospace`;
+    canvasCtx.textBaseline = "top";
+
+    startLoop();
+  });
+
+  createEffect(async () => {
+    videoEl.srcObject = camera()!;
+    videoEl.playsInline = true;
+    videoEl.muted = true;
+
+    await videoEl.play();
   });
 
   return (
