3df5ea1467a5cca174a140db6bb9d1d94b55c38f
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-01-31 20:54:46 +0000

    vai

--- crates/brightness/src/main.rs
+++ crates/brightness/src/main.rs
@@ -1,3 +1,13 @@
+#![feature(ascii_char)]
+#![feature(ascii_char_variants)]
+use std::ascii;
+
 fn main() {
-    println!("Hello, world!");
+    // ASCII printable characters range from 32 to 127.
+    // But 32 is Space, and 127 is Delete.
+    let ascii_chars: Vec<ascii::Char> = (32..127)
+        .map(|c| unsafe { ascii::Char::from_u8_unchecked(c) })
+        .collect();
+
+    dbg!(ascii_chars);
 }
