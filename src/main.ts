e1c8a049619c933ce98201708fc43fd396c4061c
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-01-31 14:34:13 +0000

    vai

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import "./styles/global.css";
 
 const ASCII_CHARS: string =
-  "@&%QWNM0gB$#DR8mHXKAUbGOpV4d9h6PkqwSE2]ayjxY5Zoen[ult13If}C{iF|(7J)vTLs?z/*cr!+<>;=^,_:'-.`";
-
-const COLOR_SPACE: PredefinedColorSpace = "srgb";
+    "@&%QWNM0gB$#DR8mHXKAUbGOpV4d9h6PkqwSE2]ayjxY5Zoen[ult13If}C{iF|(7J)vTLs?z/*cr!+<>;=^,_:'-.`",
+  COLOR_SPACE: PredefinedColorSpace = "srgb";
 
 let running: boolean = false,
   rootEl: HTMLElement,
@@ -28,22 +27,27 @@ async function init() {
   renderedCanvasCtx = renderedCanvasEl.getContext("2d", {
     colorSpace: COLOR_SPACE,
   })!;
+  renderedCanvasCtx.imageSmoothingEnabled = false;
 
   offscreenCanvasEl = document.createElement("canvas");
   offscreenCanvasCtx = offscreenCanvasEl.getContext("2d", {
     willReadFrequently: true,
     colorSpace: COLOR_SPACE,
   })!;
+  offscreenCanvasCtx.imageSmoothingEnabled = false;
 
   if (!renderedCanvasCtx || !offscreenCanvasCtx) {
     throw new Error("Canvas context not found");
   }
 
-  renderedCanvasEl.height = renderedCanvasEl.height;
-  renderedCanvasEl.width = renderedCanvasEl.width;
+  const HEIGHT = renderedCanvasEl.height / 2,
+    WIDTH = renderedCanvasEl.width / 2;
 
-  offscreenCanvasEl.height = renderedCanvasEl.height;
-  offscreenCanvasEl.width = renderedCanvasEl.width;
+  renderedCanvasEl.height = HEIGHT;
+  renderedCanvasEl.width = WIDTH;
+
+  offscreenCanvasEl.height = HEIGHT;
+  offscreenCanvasEl.width = WIDTH;
 
   stream = await navigator.mediaDevices.getUserMedia({
     video: true,
@@ -64,6 +68,11 @@ function start() {
 }
 
 function loop() {
+  renderAscii(renderOffscreen());
+  setTimeout(loop, 50);
+}
+
+function renderOffscreen(): ImageData {
   offscreenCanvasCtx.drawImage(
     offscreenVideoEl,
     0,
@@ -72,22 +81,41 @@ function loop() {
     renderedCanvasEl.height,
   );
 
-  const imageData = offscreenCanvasCtx.getImageData(
+  const image = offscreenCanvasCtx.getImageData(
     0,
     0,
     renderedCanvasEl.width,
     renderedCanvasEl.height,
   );
 
-  renderedCanvasCtx.drawImage(
-    offscreenVideoEl,
+  renderedCanvasCtx.clearRect(
     0,
     0,
     renderedCanvasEl.width,
     renderedCanvasEl.height,
   );
 
-  setTimeout(loop, 50);
+  return image;
+}
+
+function renderAscii(image: ImageData) {
+  for (let i = 0; i < image.data.length; i += 4) {
+    const r = image.data[i + 0];
+    const g = image.data[i + 1];
+    const b = image.data[i + 2];
+
+    const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+    const charIndex = Math.floor((luminance / 255) * (ASCII_CHARS.length - 1));
+    const char = ASCII_CHARS[charIndex];
+
+    renderedCanvasCtx.fillStyle = `rgb(${r},${g},${b})`;
+    renderedCanvasCtx.fillText(
+      char,
+      (i / 4) % renderedCanvasEl.width,
+      Math.floor(i / 4 / renderedCanvasEl.width),
+    );
+  }
 }
 
 async function bootstrap() {
